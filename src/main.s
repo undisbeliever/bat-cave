6833db102d97e0881c0dae6aa858016daf2b4768
Marcus Rowe <undisbeliever@gmail.com>
2015-12-09 15:50:41 +1000

    chore: Add copyright to ROM

--- src/main.s
+++ src/main.s
@@ -21,5 +21,14 @@
 	FOREVER
 .endroutine
 
+
+.segment "COPYRIGHT"
+		;1234567890123456789012345678901
+	.byte	"Bat Cave                       ", 10
+	.byte	"(c) 2015, The Undisbeliever    ", 10
+	.byte	"MIT Licensed, CC0 Graphics     ", 10
+	.byte	"One Game Per Month Challange   ", 10
+
+
 ; vim: set ft=asm:
 
